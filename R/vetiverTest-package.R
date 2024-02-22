379fa07c04f4672b98621545bd581c8fb4b8e3c7
Richard Hanna <richardshanna91@gmail.com>
2024-02-22 13:23:52 -0500

    fix

--- R/vetiverTest-package.R
+++ R/vetiverTest-package.R
@@ -1,7 +1,5 @@
 #' @keywords internal
 #' @aliases vetiverTest-package
-#' @importFrom workflows workflow add_model add_recipe
-#' @importFrom recipes step_ns recipe
 #' @importFrom parsnip set_engine linear_reg fit
 #' @importFrom vetiver handler_startup handler_predict vetiver_create_description
 #' vetiver_ptype vetiver_model
